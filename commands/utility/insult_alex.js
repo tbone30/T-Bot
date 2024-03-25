1611da9e816b543230f3f64ff15609c7e5598caf
tbone30 <twelch8585@gmail.com>
2024-03-25 17:53:10 -0400

    added support for the insult command

--- commands/utility/insult_alex.js
+++ commands/utility/insult_alex.js
@@ -2,9 +2,9 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setName('insult_alex')
+		.setDescription('Insults Alex!'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		await interaction.reply('<@443873513879633940> is a little bitch');
 	},
 };
